1337c45e5fdbe6c55aba553846ab215ba0ad77a9
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-02-16 15:06:16 +0400

    nodedev: add DevNode children and helper
    <devnode> have been proposed for libvirt 3.1, it provides device /dev/
    path when available.
    
    The get_devnode() helper will return the by-path/ path preferably, as
    they should be stable.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- virtinst/nodedev.py
+++ virtinst/nodedev.py
@@ -17,9 +17,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301 USA.
 
-import logging
+import logging, os
 
-from .xmlbuilder import XMLBuilder, XMLProperty
+from .xmlbuilder import XMLBuilder, XMLProperty, XMLChildProperty
 
 
 def _compare_int(nodedev_val, hostdev_val):
@@ -35,6 +35,13 @@ def _compare_int(nodedev_val, hostdev_val):
     nodedev_val = _intify(nodedev_val)
     hostdev_val = _intify(hostdev_val)
     return (nodedev_val == hostdev_val or hostdev_val == -1)
+
+
+class DevNode(XMLBuilder):
+    _XML_ROOT_NAME = "devnode"
+
+    node_type = XMLProperty("./@type")
+    path = XMLProperty(".")
 
 
 class NodeDevice(XMLBuilder):
@@ -104,6 +111,16 @@ class NodeDevice(XMLBuilder):
     name = XMLProperty("./name")
     parent = XMLProperty("./parent")
     device_type = XMLProperty("./capability/@type")
+    devnodes = XMLChildProperty(DevNode)
+
+    def get_devnode(self, parent="by-path"):
+        for d in self.devnodes:
+            paths = d.path.split(os.sep)
+            if len(paths) > 2 and paths[-2] == parent:
+                return d
+        if len(self.devnodes) > 0:
+            return self.devnodes[0]
+        return None
 
     def pretty_name(self):
         """
