a97196b02dee357a3b716622761baef5b1d68a63
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-02-16 15:08:56 +0400

    virtinst: add DRMDevice
    drm capability has been proposed for libvirt 3.1, it provide Direct
    Rendering Manager (DRM) devices.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- virtinst/nodedev.py
+++ virtinst/nodedev.py
@@ -53,6 +53,7 @@ class NodeDevice(XMLBuilder):
     CAPABILITY_TYPE_STORAGE = "storage"
     CAPABILITY_TYPE_SCSIBUS = "scsi_host"
     CAPABILITY_TYPE_SCSIDEV = "scsi"
+    CAPABILITY_TYPE_DRM = "drm"
 
     @staticmethod
     def lookupNodedevFromString(conn, idstring):
@@ -327,6 +328,15 @@ class SCSIBus(NodeDevice):
     wwpn = XMLProperty("./capability/capability[@type='fc_host']/wwpn")
 
 
+class DRMDevice(NodeDevice):
+    drm_type = XMLProperty("./capability/type")
+
+    def pretty_name(self, conn):
+        parent = NodeDevice.lookupNodedevFromString(conn, self.parent)
+
+        return "%s (%s)" % (parent.pretty_name(), self.drm_type)
+
+
 def _AddressStringToHostdev(conn, addrstr):
     from .devicehostdev import VirtualHostDevice
     hostdev = VirtualHostDevice(conn)
@@ -409,5 +419,7 @@ def _typeToDeviceClass(t):
         return SCSIBus
     elif t == NodeDevice.CAPABILITY_TYPE_SCSIDEV:
         return SCSIDevice
+    elif t == NodeDevice.CAPABILITY_TYPE_DRM:
+        return DRMDevice
     else:
         return NodeDevice
